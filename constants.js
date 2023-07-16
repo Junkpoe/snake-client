6ea7f2014213dc7e9a2ba368e900e5e6687c9754
DevilGahlia <superman00897@hotmail.com>
2023-07-16 02:57:00 -0300

    Added constants from input and client to make more readable

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = "local host";
+const PORT = 50541;
+const moveUpKey = 'w';
+const moveLeftKey = 'a';
+const moveDownKey = 's';
+const moveRightKey = 'd';
+const messages = {
+  'q': "Say: It's dancing time",
+  'e': "Say: WeeWooWeeWoo",
+  '1': "Say: Howdy",
+  '2': "Say: git gud",
+  '3': "Say: boop"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  moveUpKey,
+  moveLeftKey,
+  moveDownKey,
+  moveRightKey,
+  messages
+};
