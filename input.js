7f99989be7c05bf595b5972700f2d769745bfb97
DevilGahlia <superman00897@hotmail.com>
2023-07-16 02:56:03 -0300

    Refactored code and moved some code over to constants.js

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 const { connect } = require("./client");
+const { moveUpKey, moveRightKey, moveDownKey, moveLeftKey, messages } = require("./constants");
 
 let connection;
 
@@ -12,46 +13,30 @@ const setupInput = function(conn) {
   return stdin;
 };
 
-const handleUserInput = (input, conn) => { // code for reading user input
+const handleUserInput = (input) => { // code for reading user input
   if (input === '\u0003') {
     process.exit();
   }
 
-  if (input === 'w') {
+  if (input === moveUpKey) {
     connection.write("Move: up")
   };
 
-  if (input === 'd') {
+  if (input === moveRightKey) {
     connection.write("Move: right")
   };
 
-  if (input === 's') {
+  if (input === moveDownKey) {
     connection.write("Move: down")
   };
 
-  if (input === 'a') {
+  if (input === moveLeftKey) {
     connection.write("Move: left")
-  };
+  }
 
-  if (input === 'q') {
-    connection.write("Say: It's dancing time")
-  };
-
-  if (input === 'e') {
-    connection.write("Say: WeeWooWeeWoo")
-  };
-
-  if (input === '1') {
-    connection.write("Say: Howdy")
-  };
-
-  if (input === '2') {
-    connection.write("Say: git gud")
-  };
-
-  if (input === '3') {
-    connection.write("Say: boop")
-  };
+  if (messages[input]) {
+    connection.write(messages[input]);
+  }
 };
 
 module.exports = {setupInput};
