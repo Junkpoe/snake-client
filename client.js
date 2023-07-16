8b4f873266c8238f02d2d54dc1f51277a061393b
DevilGahlia <superman00897@hotmail.com>
2023-07-16 01:11:00 -0300

    Moved connect code from play.js and exported so it could be accessed

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "local host",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on ('connect', () => {
+    console.log("Succesfully connected to game");
+  });
+
+  return conn;
+};
+
+module.exports = {connect};
