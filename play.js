28397adf1b3c29cf7d7c55fa9a83000c1632710c
DevilGahlia <superman00897@hotmail.com>
2023-07-16 01:41:33 -0300

    Added code for user input to exit game upon ctrl + c

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 const {connect} = require(`./client`)
 
+// code to setup interface to handle input from user with stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (input) { // code for reading user input
+  if (input === '\u0003') {
+    process.exit();
+  }
+}
+
 console.log("Connecting ...");
+setupInput();
 connect();
 
