4abe3c87b524c8181e8c62d980ce02b325e24e3d
DevilGahlia <superman00897@hotmail.com>
2023-07-16 02:29:21 -0300

    Refactored code and added connection for accepting user input for movement using WASD

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function () {
+const { connect } = require("./client");
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,10 +12,26 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (input) { // code for reading user input
+const handleUserInput = (input, conn) => { // code for reading user input
   if (input === '\u0003') {
     process.exit();
   }
+
+  if (input === 'w') {
+    connection.write("Move: up")
+  };
+
+  if (input === 'd') {
+    connection.write("Move: right")
+  };
+
+  if (input === 's') {
+    connection.write("Move: down")
+  };
+
+  if (input === 'a') {
+    connection.write("Move: left")
+  };
 };
 
 module.exports = {setupInput};
