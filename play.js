d061da719c737b1e2aa88f157e21cf28a11acf02
DevilGahlia <superman00897@hotmail.com>
2023-07-16 01:10:18 -0300

    Refactored and moved code to client.js

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "172.23.47.215",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on ('connect', () => {
-    console.log("Succesfully connected to game");
-  });
-
-  return conn;
-};
+const {connect} = require(`./client`)
 
 console.log("Connecting ...");
 connect();
